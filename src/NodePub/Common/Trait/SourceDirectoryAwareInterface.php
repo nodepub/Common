93057949cbd52419bb233e5ffb9f45e1c2be0bb8
Andrew Gruner <andrew.gruner@gmail.com>
2014-02-11 11:17:32 -0800

    Adding SourceDirectoryAware trait and interface

---
+++ src/NodePub/Common/Trait/SourceDirectoryAwareInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace NodePub\Core\Trait;
+
+use Symfony\Component\Finder\Finder;
+
+interface SourceDirectoryAwareInterface {
+    
+    /**
+     * @var array
+     */
+    protected $sourceDirs;
+    
+    /**
+     * Adds a source directory
+     */
+    public function addSource($sourcePath, $useStrict);
+    
+    /**
+     * Searches source directories for criteria in given Finder
+     */
+    protected function findInSourceDirs(Finder $finder);
+}
---
+++ src/NodePub/Common/Trait/SourceDirectoryAwareTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace NodePub\Core\Trait;
+
+use Symfony\Component\Finder\Finder;
+
+trait SourceDirectoryAwareTrait {
+    
+    /**
+     * @var array
+     */
+    protected $sourceDirs = array();
+    
+    /**
+     * Adds a directory to the array of sources that will
+     * be searched for themes to load.
+     */
+    public function addSource($sourcePath, $useStrict=true)
+    {
+        if (is_link($sourcePath)) {
+            return $this->addSource(realpath($sourcePath), $useStrict);
+        }
+        
+        if (is_dir($sourcePath)) {
+            $this->sourceDirs[] = $sourcePath;
+        } else {
+            if (true === $useStrict) {
+                throw new \Exception(sprintf('Path {%s} is not a readable directory', $sourcePath));
+            }
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * Searches source directories for criteria in given Finder
+     *
+     * @return Finder
+     */
+    protected function findInSourceDirs(Finder $finder)
+    {
+        foreach ($this->sourceDirs as $dir) {
+            $finder->in($dir);
+        }
+
+        return $finder;
+    }
+}
